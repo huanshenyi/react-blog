f2faaf9ababccd5955f3c4c4cc6d65d0e5147bf0
huanshenyi <txy1226052@gmail.com>
2019-10-21 00:22:47 +0900

    header制作中

---
+++ src/common/header/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {HeaderWrapper} from "./style"
+
+function Header() {
+    return (
+        <HeaderWrapper>header</HeaderWrapper>
+    );
+}
+export default Header;
