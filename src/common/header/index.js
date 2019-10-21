84671472bd1419c5ee55118727b585d790ae9de8
huanshenyi <txy1226052@gmail.com>
2019-10-21 23:49:34 +0900

    header-ok

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,9 +1,30 @@
 import React from "react"
-import {HeaderWrapper} from "./style"
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
+} from "./style"
 
 function Header() {
     return (
-        <HeaderWrapper>header</HeaderWrapper>
-    );
+        <HeaderWrapper>
+            <Logo />
+            <Nav>
+                <NavItem className="left active">ホーム</NavItem>
+                <NavItem className="left">ダウンロード</NavItem>
+                <NavItem className="right">ログイン</NavItem>
+                <NavItem className="right">Aa</NavItem>
+                <NavSearch></NavSearch>
+            </Nav>
+            <Addition>
+                <Button className="writting">投稿する</Button>
+                <Button className="reg">登録</Button>
+            </Addition>
+        </HeaderWrapper>
+    )
 }
 export default Header;
