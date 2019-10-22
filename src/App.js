6777de0ef62864de1f71cfba69a1da9130df0de1
huanshenyi <txy1226052@gmail.com>
2019-10-23 00:41:03 +0900

    reduxでheader-date処理

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Fragment } from 'react';
+import { Provider } from "react-redux"
 import {GlobalStyle} from "./style"
 import {Iconfont} from "./statics/iconfont/iconfont"
 import Header from "./common/header";
+import store from "./store";
 
 function App() {
   return (
     <Fragment>
         <GlobalStyle/>
         <Iconfont/>
-        <Header/>
+        <Provider store={store}>
+            <Header/>
+        </Provider>
     </Fragment>
   );
 }
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,5 @@
-import React , { Component } from "react"
+import React from "react"
+import { connect } from "react-redux"
 import { CSSTransition } from "react-transition-group";
 import {
     HeaderWrapper,
@@ -11,60 +12,62 @@ import {
     Button
 } from "./style"
 
-class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            focused:false
-        };
-        this.handleInputFocus = this.handleInputFocus.bind(this);
-        this.handleInputBlur = this.handleInputBlur.bind(this);
+const Header = (props) => {
+   return (
+       <HeaderWrapper>
+           <Logo />
+           <Nav>
+               <NavItem className="left active">ホーム</NavItem>
+               <NavItem className="left">ダウンロード</NavItem>
+               <NavItem className="right">ログイン</NavItem>
+               <NavItem className="right">
+                   <span className="iconfont">&#xe602;</span>
+               </NavItem>
+               <SearchWrapper>
+                   <CSSTransition
+                       in={props.focused}
+                       timeout={200}
+                       classNames="slide"
+                   >
+                       <NavSearch
+                           className={props.focused ? "focused" : ""}
+                           onFocus={props.handleInputFocus}
+                           onBlur={props.handleInputBlur}
+                       ></NavSearch>
+                   </CSSTransition>
+                   <span className={props.focused ? "focused iconfont" : "iconfont"}>&#xe621;</span>
+               </SearchWrapper>
+           </Nav>
+           <Addition>
+               <Button className="writting">
+                   <span className="iconfont">&#xe601;</span>
+                   投稿する
+               </Button>
+               <Button className="reg">登録</Button>
+           </Addition>
+       </HeaderWrapper>
+   )
+};
+
+const mapStateToProps = (state)=> {
+    return {
+       focused: state.focused
     }
-    render() {
-        return (
-            <HeaderWrapper>
-                <Logo />
-                <Nav>
-                    <NavItem className="left active">ホーム</NavItem>
-                    <NavItem className="left">ダウンロード</NavItem>
-                    <NavItem className="right">ログイン</NavItem>
-                    <NavItem className="right">
-                        <span className="iconfont">&#xe602;</span>
-                    </NavItem>
-                    <SearchWrapper>
-                        <CSSTransition
-                         in={this.state.focused}
-                         timeout={200}
-                         classNames="slide"
-                        >
-                            <NavSearch
-                                className={this.state.focused ? "focused" : ""}
-                                onFocus={this.handleInputFocus}
-                                onBlur={this.handleInputBlur}
-                            ></NavSearch>
-                        </CSSTransition>
-                        <span className={this.state.focused ? "focused iconfont" : "iconfont"}>&#xe621;</span>
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className="writting">
-                        <span className="iconfont">&#xe601;</span>
-                        投稿する
-                    </Button>
-                    <Button className="reg">登録</Button>
-                </Addition>
-            </HeaderWrapper>
-        )
+};
+const mapDispathToProps = (dispatch) => {
+    return {
+        handleInputFocus(){
+          const action = {
+              type: "search_focus"
+          };
+          dispatch(action);
+        },
+        handleInputBlur(){
+            const action = {
+                type: "search_blur"
+            };
+            dispatch(action);
+        }
     }
-    handleInputFocus(){
-        this.setState({
-            focused: true
-        })
-    }
-    handleInputBlur(){
-        this.setState({
-            focused: false
-        })
-    }
-}
-export default Header;
+};
+export default connect(mapStateToProps, mapDispathToProps)(Header);
