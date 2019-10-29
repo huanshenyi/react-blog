ff3bd707bf24c51581aa2b4002b94a2b6885cc8a
huanshenyi <txy1226052@gmail.com>
2019-10-29 23:34:44 +0900

    右側内容入れた

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -9,6 +9,38 @@ const defaultState = fromJS({
         id:2,
         title:"python",
         imgUrl:"https://s3-ap-northeast-1.amazonaws.com/qiita-tag-image/a02fbc137334e63fa01e1df9b4dd0f8283a63ee8/large.jpg?1559694099"
+    }],
+    articleList:[{
+        id:1,
+        title:"Goはプログラミング言語の1つである",
+        desc:"2009年、GoogleでRobert Griesemer、ロブ・パイク、ケン・トンプソンによって設計された。Goは、静的型付け、C言語の\n" +
+            "伝統に則ったコンパイル言語、メモリ安全性、ガベージコレクション、構造的型付け、CSPスタイルの並行性などの特徴を持つ。",
+        imgUrl:"https://unit42.paloaltonetworks.com/wp-content/uploads/2019/07/golang-hacker.jpg"
+    },{
+        id:2,
+        title:"Goはプログラミング言語の1つである",
+        desc:"2009年、GoogleでRobert Griesemer、ロブ・パイク、ケン・トンプソンによって設計された。Goは、静的型付け、C言語の\n" +
+            "伝統に則ったコンパイル言語、メモリ安全性、ガベージコレクション、構造的型付け、CSPスタイルの並行性などの特徴を持つ。",
+        imgUrl:"https://unit42.paloaltonetworks.com/wp-content/uploads/2019/07/golang-hacker.jpg"
+    },{
+        id:3,
+        title:"Goはプログラミング言語の1つである",
+        desc:"2009年、GoogleでRobert Griesemer、ロブ・パイク、ケン・トンプソンによって設計された。Goは、静的型付け、C言語の\n" +
+            "伝統に則ったコンパイル言語、メモリ安全性、ガベージコレクション、構造的型付け、CSPスタイルの並行性などの特徴を持つ。",
+        imgUrl:"https://unit42.paloaltonetworks.com/wp-content/uploads/2019/07/golang-hacker.jpg"
+    },{
+        id:4,
+        title:"Goはプログラミング言語の1つである",
+        desc:"2009年、GoogleでRobert Griesemer、ロブ・パイク、ケン・トンプソンによって設計された。Goは、静的型付け、C言語の\n" +
+            "伝統に則ったコンパイル言語、メモリ安全性、ガベージコレクション、構造的型付け、CSPスタイルの並行性などの特徴を持つ。",
+        imgUrl:"https://unit42.paloaltonetworks.com/wp-content/uploads/2019/07/golang-hacker.jpg"
+    }],
+    recommendList:[{
+        id:1,
+        imgUrl:"https://tpc.googlesyndication.com/simgad/5329228355675347908"
+    },{
+        id:2,
+        imgUrl:"https://s3-ap-northeast-1.amazonaws.com/hodalog/wp-content/uploads/2019/03/20211955/golang-icach-700x467.png"
     }]
 });
 
