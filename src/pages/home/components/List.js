4fe5450aa55baec5bdc316724310cb3e0a7d4217
huanshenyi <txy1226052@gmail.com>
2019-11-02 12:53:09 +0900

    loadmoreadd

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,16 +1,17 @@
 import React, {Component} from "react";
 import { ListItem, ListInfo, LoadMore } from "../style";
 import { connect } from "react-redux";
+import { actionCreators } from "../store"
 
 class List extends Component{
     render() {
-        const { list } = this.props;
+        const { list, getMoreList, page} = this.props;
         return (
            <div>
                {
-                   list.map((item) => {
+                   list.map((item, index) => {
                        return (
-                           <ListItem key={item.get("id")}>
+                           <ListItem key={index}>
                                <img
                                    className="pic"
                                    alt="img"
@@ -23,18 +24,19 @@ class List extends Component{
                        )
                    })
                }
-               <LoadMore onClick={}>MoreInfo</LoadMore>
+               <LoadMore onClick={() =>getMoreList(page)}>MoreInfo</LoadMore>
            </div>
         )
     }
 }
 const mapStateToProps = (state) =>({
-   list:state.getIn(["home", "articleList"])
+   list: state.getIn(["home", "articleList"]),
+   page: state.getIn(["home", "articlePage"])
 });
 
 const mapDispatch = (dispatch)=>({
-   getMoreList(){
-
+   getMoreList(page){
+      dispatch(actionCreators.getMoreList(page))
    }
 });
-export default connect(mapStateToProps)(List)
+export default connect(mapStateToProps, mapDispatch)(List)
--- src/pages/home/store/actionCreators.js
+++ src/pages/home/store/actionCreators.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 import * as constants from "./constants"
+import { fromJS } from "immutable";
+
 
 const changHomeData = (result)=>({
     type: constants.CHANGE_HOME_DATA,
     topicList: result.topicList,
     articleList: result.articleList,
     recommendList: result.recommendList
+});
+//ページネーション使用する場合
+//const addHomeList = (result, nextPage)=>({
+const addHomeList = (result)=>({
+   type: constants.ADD_HOME_LIST,
+   list: fromJS(result)
+    //ページネーション使用する場合
+   //nextPage
 });
 
 export const getHomeInfo = () => {
@@ -14,6 +24,21 @@ export const getHomeInfo = () => {
           const result = res.data.data;
           const action = changHomeData(result);
           dispatch(action);
+      });
+  }
+};
+
+// page->ページネーション
+export const getMoreList = (page)=> {
+  return (dispatch)=>{
+      //もしページネーション使用する場合
+      //axios.get("http://127.0.0.1:8000/api/homeList.json?page=" + page).
+      axios.get("http://127.0.0.1:8000/api/homeList.json").then((res)=>{
+          const result = res.data.data;
+          // もしページネーション使用する場合
+          // dispatch(addHomeList(result, page + 1))
+          const action = addHomeList(result);
+          dispatch(action)
       });
   }
 };
